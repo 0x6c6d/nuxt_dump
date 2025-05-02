3e26bb7304e327c1739d990f644e64765dfe5589
0x6c6d <lucas_menke@icloud.com>
2025-05-02 17:16:23 +0200

    + basic config

---
+++ run_tracker/components/site/hero-centered.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Welcome to Your Run Tracking App</h1>
+        <p class="py-6">
+          Track your runs today. Start managing your training sessions with ease
+          and clarity.
+        </p>
+        <NuxtLink to="login" class="btn btn-primary">Get Started</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ run_tracker/components/site/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Login now!</h1>
+        <p class="py-6">
+          Only an email is required. We will send you an OTP to your mail
+          address.
+        </p>
+      </div>
+      <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div class="card-body">
+          <fieldset class="fieldset">
+            <label class="label">Email</label>
+            <input type="email" class="input" placeholder="Email" />
+            <button class="btn btn-neutral mt-4">Send OTP</button>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
